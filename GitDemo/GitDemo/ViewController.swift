016cfd740f475523559c7c2e424e4b61fff6bad6
Alok K. Tiwari <atiwari02@xento.com>
2019-03-04 11:19:23 +0530

    First Commit

---
+++ GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Alok K. Tiwari on 04/03/19.
+//  Copyright © 2019 Alok K. Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
